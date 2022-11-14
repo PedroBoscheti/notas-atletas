30f3bef170039d31bab8caf9e4dc99b75b54de3f
PedroBoscheti <109442294+PedroBoscheti@users.noreply.github.com>
2022-11-14 18:10:36 -0300

    Add files via upload

---
+++ notas-Ginastas.js
@@ -0,0 +1,35 @@
+let ginastas = [
+  {
+    nome: "Cesar Abascal",
+    notas: [10, 9.34, 8.42, 10, 7.88]
+  },
+  {
+    nome: "Fernando Puntel",
+    notas:  [8, 10, 10, 7, 9.33]
+  },
+  {
+    nome: "Daiane Jelinsky",
+    notas: [7, 10, 9.5, 9.5, 8]
+  },
+  {
+    nome: "Bruno Castro",
+    notas: [10, 10, 10, 9, 9.5]
+  }
+ ];
+ 
+ for (let i = 0; i < ginastas.length; i++){
+ 
+   let notas = ginastas[i].notas;
+   notas = notas.sort();
+   notas = notas.slice(1,4);
+ 
+   let somaNota = notas.reduce(function(total, nota){
+     return total + nota;  
+   },0);
+ 
+   let notaValida = somaNota / notas.length;
+ 
+   console.log("Ginasta: " + ginastas[i].nome);
+   console.log("Recebeu: " + ginastas[i].notas);
+   console.log("Resultado: " + notaValida); 
+ }
